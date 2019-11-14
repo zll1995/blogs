56f069ccedaf77de20580eb6c183c11ccb5f1185
zhengjinhu <939785976@qq.com>
2019-11-14 21:59:55 +0800

    登录注册页面

--- springboot-entity/src/main/java/com/jk/user/model/User.java
+++ springboot-entity/src/main/java/com/jk/user/model/User.java
@@ -1,8 +1,30 @@
 package com.jk.user.model;
 
+import lombok.Data;
+
 import java.io.Serializable;
 
+@Data
 public class User implements Serializable {
 
-    private static final long serialVersionUID = -35828233809885660L;
+    private static final long serialVersionUID = 5034426654100868124L;
+
+    private Integer userId;
+
+    private String userName;
+
+    private String password;
+
+    private String headImg;
+
+    private String phone;
+
+    private String name; //博客昵称
+
+    private String blogName; //博客标题
+
+    private String blogUrl;  //博客地址
+
+    private String email;  //电子邮箱
+
 }
