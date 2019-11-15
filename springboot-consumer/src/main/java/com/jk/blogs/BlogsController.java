1f3afe870ddf5132e95d31b34e5f3e407fd46a7a
lz5201314 <3428398622@qq.com>
2019-11-15 11:30:28 +0800

    查询所有文章2

--- springboot-consumer/src/main/java/com/jk/blogs/BlogsController.java
+++ springboot-consumer/src/main/java/com/jk/blogs/BlogsController.java
@@ -1,11 +1,31 @@
 package com.jk.blogs;
 
 import com.alibaba.dubbo.config.annotation.Reference;
+import com.jk.blogs.model.Zhang;
 import com.jk.blogs.service.BlogsService;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Map;
 
 @Controller
 public class BlogsController {
     @Reference(version = "1.0")
     private BlogsService blogsService;
+
+
+    @RequestMapping("queryzhang")
+    public String queryzhang(){
+
+        return "show";
+    }
+//博客文章查询
+    @RequestMapping("queryzhangAll")
+    @ResponseBody
+    public Map<String,Object> queryzhangAll(Zhang z, int page, int rows){
+        return blogsService.queryzhangAll(z,page,rows);
+
+    }
+
 }
